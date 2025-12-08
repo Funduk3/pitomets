b3cd27376ee26c5f8f67dd469b75efc8f4d78c2e
artemsharkov <artemshar48@gmail.com>
2025-12-08 13:47:44 +0300

    1

--- monolit/src/main/kotlin/com/pitomets/monolit/config/SecurityConfig.kt
+++ monolit/src/main/kotlin/com/pitomets/monolit/config/SecurityConfig.kt
@@ -79,6 +79,7 @@ class SecurityConfig(
                     "/refresh"
                 ).permitAll()
                 it.requestMatchers(HttpMethod.GET, "/listings/**").permitAll()
+                it.requestMatchers("/listings/**").hasRole("SELLER")
                 it.requestMatchers("/seller/profile").authenticated() // Создание профиля для всех
                 it.requestMatchers("/seller/**").hasRole("SELLER")
                 it.anyRequest().authenticated()
