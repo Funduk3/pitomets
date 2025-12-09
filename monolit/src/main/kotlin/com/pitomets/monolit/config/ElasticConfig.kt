7024422484d914f61b503b06bd6d5c16a32f1ae4
artemsharkov <artemshar48@gmail.com>
2025-12-09 15:58:30 +0300

    1

--- monolit/src/main/kotlin/com/pitomets/monolit/config/ElasticConfig.kt
+++ monolit/src/main/kotlin/com/pitomets/monolit/config/ElasticConfig.kt
@@ -3,16 +3,19 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper
 import co.elastic.clients.transport.rest_client.RestClientTransport
 import org.apache.http.HttpHost
 import org.elasticsearch.client.RestClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class ElasticConfig {
+class ElasticConfig(
+    @Value("\${elasticsearch.host}") private val host: Int,
+) {
 
     @Bean
     fun elasticClient(): ElasticsearchClient {
         val restClient = RestClient.builder(
-            HttpHost("localhost", Int.MAX_VALUE, "http") // поправить
+            HttpHost("localhost", host, "http")
         ).build()
 
         val transport = RestClientTransport(
