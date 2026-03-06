48cdd07e8a3485c1678f03945accd39bd8bb2920
Fedor Demin <119737017+Funduk3@users.noreply.github.com>
2026-03-06 16:48:10 +0300

    moderation_service (#65)
    Co-authored-by: fedordemin <fedordemin13@gmail.com>
    Co-authored-by: Sharkov Artem <artemshar48@gmail.com>

--- moderator/src/main/kotlin/com/pitomets/moderator/infrastructure/dto/ModeriumAnalyzeResponse.kt
+++ moderator/src/main/kotlin/com/pitomets/moderator/infrastructure/dto/ModeriumAnalyzeResponse.kt
@@ -1,8 +1,5 @@
 package com.pitomets.moderator.infrastructure.dto
 
-import com.pitomets.moderator.infrastructure.client.ModeriumCategories
-import com.pitomets.moderator.infrastructure.client.ModeriumDecision
-
 data class ModeriumAnalyzeResponse(
     val decision: ModeriumDecision? = null,
     val categories: ModeriumCategories? = null,
--- moderator/src/main/kotlin/com/pitomets/moderator/infrastructure/dto/ModeriumCategories.kt
+++ moderator/src/main/kotlin/com/pitomets/moderator/infrastructure/dto/ModeriumCategories.kt
@@ -1,8 +1,6 @@
 package com.pitomets.moderator.infrastructure.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.pitomets.moderator.infrastructure.client.ModeriumCategoryMatches
-import com.pitomets.moderator.infrastructure.client.ModeriumToxicity
 
 data class ModeriumCategories(
     val profanity: ModeriumCategoryMatches? = null,
